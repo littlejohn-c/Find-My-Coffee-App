8eb9483285cb384fecfdf2e246506f3a7c7fcdb2
Carl <99614187+carl-baby@users.noreply.github.com>
2023-01-24 23:44:36 -0600

    wrapping up before i mess up

--- app/src/main/java/com/example/simple_login/CreateUser.java
+++ app/src/main/java/com/example/simple_login/CreateUser.java
@@ -4,39 +4,73 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 
+import org.w3c.dom.Text;
+
 public class CreateUser extends AppCompatActivity {
-    Button confirmbtn;
+    EditText username, password, email;
+    Button btnConfirm;
+    DBHelper DB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_user);
 
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        email = findViewById(R.id.email);
+        btnConfirm = findViewById(R.id.btnConfirm);
+        DB = new DBHelper(this);
 
-        confirmbtn = findViewById(R.id.confirmbtn);
-        confirmbtn.setOnClickListener(new View.OnClickListener() {
+
+        btnConfirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                finish();
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+                String mail = email.getText().toString();
+                if(TextUtils.isEmpty(user) || TextUtils.isEmpty(pass) || TextUtils.isEmpty(mail))
+                    Toast.makeText(CreateUser.this, "All fields required!", Toast.LENGTH_SHORT).show();
+                else {
+                    Boolean checkUser = DB.checkUsername(user);
+                    if (checkUser == false) {
+                        Boolean insert = DB.insertData(user, pass, String.valueOf(email));
+                        if (insert == true) {
+                            Toast.makeText(CreateUser.this, "Registered Successfully!", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(), MainActivityLogin.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(CreateUser.this, "Registration Failed. Try Again.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
             }
         });
 
-        TextView username = (TextView) findViewById(R.id.username);
-        TextView password = (TextView) findViewById(R.id.password);
-        TextView email = (TextView) findViewById(R.id.email);
 
-        MaterialButton confirmbtn = (MaterialButton) findViewById(R.id.confirmbtn);
+        btnConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),MainActivityLogin.class);
+                startActivity(intent);
+            }
+        });
 
-    }
-
-    private void switchActivities() {
-        Intent switchActivityIntent = new Intent(this, MainActivityLogin.class);
-        startActivity(switchActivityIntent);
-        }
-}
+//        MaterialButton confirmbtn = (MaterialButton) findViewById(R.id.btnConfirm);
+//
+//    }
+//
+//    private void switchActivities() {
+//        Intent switchActivityIntent = new Intent(this, MainActivityLogin.class);
+//        startActivity(switchActivityIntent);
+//        }
+    }}
--- app/src/main/java/com/example/simple_login/MainActivityLogin.java
+++ app/src/main/java/com/example/simple_login/MainActivityLogin.java
@@ -2,51 +2,72 @@ package com.example.simple_login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.android.material.button.MaterialButton;
 
 public class MainActivityLogin extends AppCompatActivity {
-    Button createbtn;
+    TextView username, password, email;
+    Button btnLogin;
+    DBHelper DB;
 
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_login);
 
-        createbtn = findViewById(R.id.newUserButton);
-        createbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                switchActivities();
-            }
-        });
+        username = findViewById(R.id.username1);
+        password = findViewById(R.id.password1);
+        btnLogin = findViewById(R.id.btnLogin1);
+        DB = new DBHelper(this);
 
-        TextView username = (TextView) findViewById(R.id.username);
-        TextView password = (TextView) findViewById(R.id.password);
-
-        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
-        MaterialButton newUser = (MaterialButton)  findViewById(R.id.newUserButton);
-
-        //admin and admin
-
-        loginbtn.setOnClickListener(new View.OnClickListener() {
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                Intent i = new Intent(getApplicationContext(),CoffeeShopListActivity.class);
-                startActivity(i);
-
             }
         });
-    }
 
-    private void switchActivities() {
-        Intent switchActivityIntent = new Intent(this, CreateUser.class);
-        startActivity(switchActivityIntent);
+
+//        btnLogin = findViewById(R.id.newUserButton);
+//        btnLogin.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                switchActivities();
+//            }
+//        });
+//
+//
+//
+//        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
+//        MaterialButton newUser = (MaterialButton)  findViewById(R.id.newUserButton);
+//
+//        //admin and admin
+//
+//        loginbtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//                Intent i = new Intent(getApplicationContext(),CoffeeShopListActivity.class);
+//                startActivity(i);
+//
+//            }
+//        });
+//    }
+//
+//    private void switchActivities() {
+//        Intent switchActivityIntent = new Intent(this, CreateUser.class);
+//        startActivity(switchActivityIntent);
+//    }
+
+
     }
 }
